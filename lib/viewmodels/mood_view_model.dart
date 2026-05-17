3543879ac699132f516685af4f34262d671cf710
asmaa sallam <asmaasallam1998@gmail.com>
2026-05-17 12:22:29 +0300

    add mood model and viewmodel

--- lib/viewmodels/mood_view_model.dart
+++ lib/viewmodels/mood_view_model.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import '../models/mood_entry.dart';
 
 class MoodViewModel extends ChangeNotifier {
-  final List<MoodEntry> moods = [];
+  final List<MoodEntry> _moods = [];
+  List<MoodEntry> get moods => _moods;
 
   void addMood(String mood) {
-    moods.insert(0, MoodEntry(mood: mood, date: DateTime.now()));
+    _moods.insert(0, MoodEntry(mood: mood, date: DateTime.now()));
+
+    if (_moods.length > 7) {
+      _moods.removeLast();
+    }
 
     notifyListeners();
   }
